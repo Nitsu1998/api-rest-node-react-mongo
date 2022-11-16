5812fc05c1d1f8f83b7307b75ceadbadce1434c6
Agustin Micheletti <91162872+Nitsu1998@users.noreply.github.com>
2022-11-16 16:28:09 -0300

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -4,7 +4,7 @@ export default {
   PORT: process.env.PORT || 8080,
   DB_URL: process.env.DB_URL,
   CORS: {
-    origin: "http://localhost:3000",
+    origin: "*",
     methods: "GET, POST, PUT, DELETE",
     credentials: true,
   },
